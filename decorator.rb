612db0455a8b840928325d7172ef3d98d42e0b82
oguntayomicheal <oguntayomicheal@gmail.com>
2023-03-07 20:48:14 +0100

    Add TrimmerDecorator class

--- decorator.rb
+++ decorator.rb
@@ -17,3 +17,9 @@ class CapitalizeDecorator < BaseDecorator
         @nameable.correct_name.capitalize()
     end
 end
+
+class TrimmerDecorator < BaseDecorator
+    def correct_name
+        @nameable.correct_name.size() > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name 
+    end
+end
