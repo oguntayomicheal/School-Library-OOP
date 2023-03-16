fc92cde9f1d2c9e2e37d9152f28a83d61983eeff
oguntayomicheal <oguntayomicheal@gmail.com>
2023-03-16 01:07:58 +0100

    Add test to methods in Person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../person'
+# require_relative '../rental'
+
+describe Person do
+  before :each do
+    @new_person = Person.new(40, 'Selma')
+  end
+
+  context 'When testing the person class' do
+    it 'Inserted data should match attributes' do
+        age = @new_person.age
+        name = @new_person.name
+    
+        expect(age).to eql 40
+        expect(name).to eql 'Selma'
+    end
+
+    it 'Return correct name' do
+        expect(@new_person.correct_name).to eq 'Selma' 
+    end
+
+    it 'Check if person can use service' do
+       expect(@new_person.can_use_services?).to eq true
+    end
+
+
+  end
+ 
+end
