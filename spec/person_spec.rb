e8e8d6d7667f13bc9b3cbb62b08fdcc67a19287f
oguntayomicheal <oguntayomicheal@gmail.com>
2023-03-16 02:17:12 +0100

    Add test for methods in Rental class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,22 +7,19 @@ describe Person do
 
   context 'When testing the person class' do
     it 'Inserted data should match attributes' do
-        age = @new_person.age
-        name = @new_person.name
-    
-        expect(age).to eql 40
-        expect(name).to eql 'Selma'
+      age = @new_person.age
+      name = @new_person.name
+
+      expect(age).to eql 40
+      expect(name).to eql 'Selma'
     end
 
     it 'Return correct name' do
-        expect(@new_person.correct_name).to eq 'Selma' 
+      expect(@new_person.correct_name).to eq 'Selma'
     end
 
     it 'Check if person can use service' do
-       expect(@new_person.can_use_services?).to eq true
+      expect(@new_person.can_use_services?).to eq true
     end
-
-
   end
- 
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,28 +2,27 @@ require_relative '../student'
 require_relative '../classroom'
 
 describe Student do
-    before :each do
-        @new_classroom = Classroom.new('Secondary')
-        @new_student = Student.new(15, 'Selma', @new_classroom)
+  before :each do
+    @new_classroom = Classroom.new('Secondary')
+    @new_student = Student.new(15, 'Selma', @new_classroom)
+  end
+
+  context 'While testing the Student class' do
+    it 'Student should inherit from Person class' do
+      expect(@new_student).to be_kind_of(Person)
     end
 
-    context 'While testing the Student class' do
-        it 'Student should inherit from Person class' do
-            expect(@new_student).to be_kind_of(Person)
-        end
-
-        it 'Check classroom attribute in Student' do
-            expect(@new_student.classroom.label).to eq 'Secondary'
-        end
-
-        it 'test the play_hocky method' do
-            expect(@new_student.play_hooky).to eql '¯\(ツ)/¯'
-        end
-
-        it 'classrooms setter should populate @students array' do
-            @new_student.classroom=@new_classroom
-            expect(@new_classroom.students.length).to eql(1)
-          end
-
+    it 'Check classroom attribute in Student' do
+      expect(@new_student.classroom.label).to eq 'Secondary'
     end
+
+    it 'test the play_hocky method' do
+      expect(@new_student.play_hooky).to eql '¯\(ツ)/¯'
+    end
+
+    it 'classrooms setter should populate @students array' do
+      @new_student.classroom = @new_classroom
+      expect(@new_classroom.students.length).to eql(1)
+    end
+  end
 end
